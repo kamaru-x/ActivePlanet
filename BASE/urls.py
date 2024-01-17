ee2954039609bc1a047ae32d1766f16c99fee3ff
Kamaru <xin.kamaru@gmail.com>
2024-01-17 07:31:11 +0530

    /// media file error fixed ///

--- BASE/urls.py
+++ BASE/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls import handler400,handler403,handler404,handler500
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('db/', admin.site.urls),
@@ -27,6 +28,9 @@ urlpatterns = [
     path('error/',include('ErrHandler.urls')),
     path('admin/',include('Core.urls')),
     path('',include('Frontpage.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
